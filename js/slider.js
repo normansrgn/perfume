cc346d123083f92314e2055371b9e3f69b424167
normansrgn <normanmagic2004@gmail.com>
2024-04-27 15:30:10 +0300

    $

---
+++ js/slider.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints:  {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 2,
+        //   spaceBetween: 
+        },
+
+        350: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+
+        780: {
+            slidesPerView: 2,
+            spaceBetween:20
+        },
+
+        920 : {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+
+        // when window width is >= 480px
+        1187: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1700: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+      }
+  });
